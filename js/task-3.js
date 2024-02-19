b4aba98c0b1525e7470cf3bc5506f600eac34f23
Mahuliii <148713111+Mahuliii@users.noreply.github.com>
2024-02-19 21:58:37 +0200

    task-3

---
+++ js/task-3.js
@@ -0,0 +1,28 @@
+class StringBuilder {
+  #value;
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+  getValue() {
+    return this.#value;
+  }
+  padEnd(str) {
+    this.#value = this.#value.padEnd(str.length + this.#value.length, str);
+  }
+  padStart(str) {
+    this.#value = this.#value.padStart(str.length + this.#value.length, str);
+  }
+  padBoth(str) {
+    this.#value = this.#value.padStart(str.length + this.#value.length, str);
+    this.#value = this.#value.padEnd(str.length + this.#value.length, str);
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
